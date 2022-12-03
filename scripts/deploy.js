faf8671fbbb915c987f7499a75eed886372b2008
sumit-mahajan <sumitmahajan2001@gmail.com>
2022-12-03 18:10:48 +0530

    Added contracts

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,7 +6,51 @@ async function main() {
   await launchTokenApp.deployed();
 
   console.log("contract address: ", launchTokenApp.address);
+
+  await testFunction(launchTokenApp);
 }
+
+const testFunction = async () => {
+  const accounts = await ethers.getSigner();
+
+  await launchTokenApp.createToken(
+    "image",
+    "name",
+    "SBF",
+    1000,
+    10000,
+    false,
+    false,
+    [false, 0, false]
+  );
+
+  await launchTokenApp.createToken(
+    "image",
+    "name",
+    "SBF2",
+    1000,
+    10000,
+    false,
+    false,
+    [false, 0, false]
+  );
+
+  const tokens = await launchTokenApp.getAllTokensForUser(accounts[0]);
+
+  console.log(tokens[0]);
+  console.log(tokens[1]);
+
+  await launchTokenApp.createPool(
+    tokens[0].contractAddress,
+    tokens[1].contractAddress
+  );
+
+  const pools = await launchTokenApp.getAllPoolsForToken(
+    tokens[0].contractAddress
+  );
+
+  console.log(pools[0]);
+};
 
 main()
   .then(() => process.exit(0))
