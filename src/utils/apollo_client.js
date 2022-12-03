87e576608d4e3e9ee06af9a447e3576a1e24fb45
sumit-mahajan <sumitmahajan2001@gmail.com>
2022-12-04 00:32:01 +0530

    Refactored gql queries

--- src/utils/apollo_client.js
+++ src/utils/apollo_client.js
@@ -1,64 +1,82 @@
-import { ApolloClient, ApolloLink } from "@apollo/client";
-import { createHttpLink } from "apollo-link-http";
+import {
+  ApolloClient,
+  ApolloLink,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { MultiAPILink } from "@habx/apollo-multi-endpoint-link";
+import { gql } from "@apollo/client";
 
-const client = new ApolloClient({
-  link: ApolloLink.from([
-    new MultiAPILink({
-      endpoints: {
-        compound:
-          "https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2",
-        balancer:
-          "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer",
-        bancor: "https://api.thegraph.com/subgraphs/name/blocklytics/bancor",
-        synthetix:
-          "https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix",
-        uniswap: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
-      },
-      createHttpLink: () => createHttpLink(),
-    }),
-  ]),
+// import { createHttpLink } from "apollo-link-http";
+
+// export const client = new ApolloClient({
+//   link: ApolloLink.from([
+//     new MultiAPILink({
+//       endpoints: {
+//         compound:
+//           "https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2",
+//         balancer:
+//           "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer",
+//         bancor: "https://api.thegraph.com/subgraphs/name/blocklytics/bancor",
+//         synthetix:
+//           "https://api.thegraph.com/subgraphs/name/synthetixio-team/synthetix",
+//         uniswap: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+//       },
+//       createHttpLink: () => createHttpLink(),
+//     }),
+//   ]),
+//   cache: InMemoryCache(),
+// });
+
+export const client = new ApolloClient({
+  uri: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2",
+  cache: new InMemoryCache(),
 });
 
-const UNISWAP_QUERY = `
-{
-    users(orderBy: id, orderDirection:desc, first: 1000) {
-      id
-    }
-}
-`;
+// const { loading, error, data } = useQuery(UNISWAP_QUERY,{variables: {first: 1000}});
 
-const BALANCER_QUERY = `
-{
-    users(orderBy: id, orderDirection:desc, first: 1000) {
-      id
-    }
-}
-`;
+// console.log(data.users.map((res) => res.id));
 
-const SYNTHETIX_QUERY = `
-{
-    snxholders(where: {balanceOf_gt: 1}, first:1000) {
-      id
-      balanceOf
-    }
-}
-`;
-
-const BANCOR_QUERY = `
-{
-    users(where: {numSwaps_gt:5}, first:1000) {
+const UNISWAP_QUERY = gql`
+  query Uniswap($first: Int!) {
+    users(orderBy: id, orderDirection: desc, first: $first) {
       id
     }
   }
 `;
 
-const COMPOUND_QUERY = `
-{
-    accountCTokens(where: {totalUnderlyingBorrowed_gt: 0}, first: 1000){
-      account{
+const BALANCER_QUERY = gql`
+  query Uniswap($first: Int!) {
+    users(orderBy: id, orderDirection: desc, first: $first) {
+      id
+    }
+  }
+`;
+
+const SYNTHETIX_QUERY = gql`
+  query Uniswap($first: Int!) {
+    snxholders(where: { balanceOf_gt: 1 }, first: $first) {
+      id
+      balanceOf
+    }
+  }
+`;
+
+const BANCOR_QUERY = gql`
+  query Uniswap($first: Int!) {
+    users(where: { numSwaps_gt: 5 }, first: $first) {
+      id
+    }
+  }
+`;
+
+const COMPOUND_QUERY = gql`
+  query Uniswap($first: Int!) {
+    accountCTokens(where: { totalUnderlyingBorrowed_gt: 0 }, first: $first) {
+      account {
         id
       }
       totalUnderlyingBorrowed
     }
-}
+  }
 `;
